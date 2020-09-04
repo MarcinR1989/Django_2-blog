914614e2105ba6568384653d1b04bb818c03bd20
MarcinR1989 <rykaczewskim@gmail.com>
2020-09-04 22:26:39 +0200

    Added urls,css and templates

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -39,3 +40,13 @@ class Post(models.Model):
 
     objects = models.Manager()  # Menedżer domyślny
     published = PublishedManager()  # Menedżer niestandardowy
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[
+                           self.publish.year,
+                           self.publish.strftime('%m'),
+                           self.publish.strftime('%d'),
+                           self.slug
+                       ]
+                       )
